82ce1a6d81475ff88b3f2fb7b860e376a48b6b7e
kaustubh2020 <kaustubhjaiswal200@gamil.com>
2023-07-30 14:06:37 +0530

    StatusBar

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-import { View, ScrollView } from "react-native";
+import { View, ScrollView, StatusBar, Button } from "react-native";
+import { useState } from "react";
+
 import Text_Image from "./Text_Image";
 import Buttons from "./Buttons";
 import Modals from "./Modals";
@@ -6,9 +8,25 @@ import Modals from "./Modals";
 const logo = require("./assets/adaptive-icon.png");
 
 export default function App() {
+  const [setstatusBarVisibility, setSetstatusBarVisibility] = useState(false);
   return (
     <ScrollView>
       <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+        <StatusBar
+          backgroundColor="midnightblue"
+          barStyle={"light-content"}
+          hidden={setstatusBarVisibility}
+        />
+        <Button
+          title="hide status bar"
+          color="orange"
+          onPress={() => setSetstatusBarVisibility(true)}
+        />
+        <Button
+          title="show status bar"
+          color="red"
+          onPress={() => setSetstatusBarVisibility(false)}
+        />
         <Buttons />
         <Modals />
         <Text_Image />
